f9f6a888af04fe1d3323947e9c7689ff5bd56760
ahmrd2222 <alhalyly1020@gmail.com>
2025-06-01 13:55:31 -0400

    Update events.js

--- src/data/events.js
+++ src/data/events.js
@@ -1,5 +1,5 @@
 const eventsData = {
-  title: "Event Photographyzzzz",
+  title: "Event Photograz",
   description: "Capturing special moments from life's most important celebrations",
   coverImage: "https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   galleries: [
